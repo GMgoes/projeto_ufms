b890e73227b9213f1f98e7f5a3ea86d256699e77
gusta <ggoes269@gmail.com>
2022-06-22 07:52:40 -0400

    first commit

---
+++ src/main/java/com/projeto/videoaula/VideoaulaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.videoaula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoaulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoaulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/videoaula/VideoaulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.videoaula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoaulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
