f4fedf8e63224718bde1121bb1807ba023aeeda4
gusta <ggoes269@gmail.com>
2022-06-22 10:49:15 -0400

    atualizacao importacoes

--- src/main/java/com/projeto/videoaula/RepositorioAgenda.java
+++ src/main/java/com/projeto/videoaula/RepositorioAgenda.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "contatos", path = "contatos")
-public class RepositorioAgenda {
+public interface RepositorioAgenda extends JpaRepository<Pessoa, Long> {
     
 }
